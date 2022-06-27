59c350f642bc3a3816bac9f66a9f44947bf38b27
Reda Tag <rtag@ncgr.gov.sa>
2022-06-28 01:19:09 +0200

    learn jsx in react

--- src/App.js
+++ src/App.js
@@ -2,21 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleNameChange = () => {
+    const names = ["Reda", "Malek", "Talia"];
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <p> Hello {handleNameChange()}</p>
+         
       </header>
     </div>
   );
