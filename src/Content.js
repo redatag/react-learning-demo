09847c483a78293b658f0927304acca4745d7c8d
Reda Tag <rtag@ncgr.gov.sa>
2022-06-28 02:44:04 +0200

    add clich handler

--- src/Content.js
+++ src/Content.js
@@ -6,9 +6,26 @@ const Content = () => {
         const int = Math.floor(Math.random() * 3);
         return names[int];
       }
+      const handleClick = () => {
+          console.log('clicked');
+      }
+      
+      const handleClick2 = (name) => {
+          console.log(`${name} was clicked`);
+      }
+      
+      const handleClick3 = (e) => {
+        //   console.log(e);
+          console.log(e.target.innerText);
+      }
+
+//Alt + Shift + arrow Down == Copy and past
     return (
         <main>
-            <p> Hello {handleNameChange()}</p>
+            <p onDoubleClick = {handleClick}> Hello {handleNameChange()}</p>
+            <button onClick={handleClick}>Click Me </button>
+            <button onClick={() => {handleClick2('Reda')}}>Click Me </button>
+            <button onClick={(e) => handleClick3(e)}>Click Me </button>
         </main>
     )
 }
