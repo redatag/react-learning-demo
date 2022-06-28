d3a2b9f3684808cb031e2581d392119688761c03
Reda Tag <rtag@ncgr.gov.sa>
2022-06-28 11:55:02 +0200

    useState Hook

--- src/Content.js
+++ src/Content.js
@@ -1,13 +1,24 @@
 import React from 'react'
+import {useState} from 'react'
 
 const Content = () => {
+    //Array desstruction
+    //first value 'name' is the current state  and 2nd element is the setting the state
+    //useState() contain the default or original state when the component load
+    const [name, setName] = useState('Reda');
+    const [count, setCount] = useState(0);
+    //we use const to never reassign the name directly 
+    //, we never need to directly modify the state of the component
+    // but we use setName to change the state 
     const handleNameChange = () => {
         const names = ["Reda", "Malek", "Talia"];
         const int = Math.floor(Math.random() * 3);
-        return names[int];
+        // return names[int];
+        setName(names[int]);
       }
       const handleClick = () => {
-          console.log('clicked');
+          setCount(count + 1);
+          console.log(`button clicked ${count} times`);
       }
       
       const handleClick2 = (name) => {
@@ -22,9 +33,11 @@ const Content = () => {
 //Alt + Shift + arrow Down == Copy and past
     return (
         <main>
-            <p onDoubleClick = {handleClick}> Hello {handleNameChange()}</p>
+            {/* <p onDoubleClick = {handleClick}> Hello {handleNameChange()}</p> */}
+            <p onDoubleClick = {handleClick}> Hello {name}</p>
+            <button onClick={handleNameChange}>Change Name </button>
+            {/* <button onClick={() => {handleClick2('Reda')}}>Click Me </button> */}
             <button onClick={handleClick}>Click Me </button>
-            <button onClick={() => {handleClick2('Reda')}}>Click Me </button>
             <button onClick={(e) => handleClick3(e)}>Click Me </button>
         </main>
     )
